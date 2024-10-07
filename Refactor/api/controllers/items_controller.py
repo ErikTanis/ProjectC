3a148be51dc0f13fa6a553126ce7fcaa7e61f537
Azif Rosan <azif_ownage@hotmail.com>
2024-10-07 14:43:37 +0200

    added not found, and refactored function parameters to be consistent

--- Refactor/api/controllers/items_controller.py
+++ Refactor/api/controllers/items_controller.py
@@ -13,11 +13,20 @@ class ItemsController(BaseController):
             case 1:
                 self.get_all(handler)
             case 2:
-                self.get_by_id(path[1], handler)
+                item_id = int(path[1])
+                self.get_by_id(handler, item_id)
             case 3:
-                self.get_inventory_by_item_id(path[1], handler)
+                if path[2] == "inventory":
+                    item_id = int(path[1])
+                    self.get_inventory_by_item_id(handler, item_id)
+                else:
+                    self.respond_not_found(handler)
             case 4:
-                self.get_inventory_totals_by_item_id(path[1], handler)
+                if path[2] == "inventory" and path[3] == "totals":
+                    item_id = int(path[1])
+                    self.get_inventory_totals_by_item_id(handler, item_id)
+                else:
+                    self.respond_not_found(handler)
             case _:
                 self.respond_not_found(handler)
 
@@ -25,10 +34,12 @@ class ItemsController(BaseController):
         self.add(handler)
 
     def handle_put_version_1(self, path, handler):
-        self.update(path, handler)
+        item_id = int(path[1])
+        self.update(handler, item_id)
 
     def handle_delete_version_1(self, path, handler):
-        self.delete(path, handler)
+        item_id = int(path[1])
+        self.delete(handler, item_id)
 
     # /api/v1/items
     def get_all(self, handler):
@@ -36,17 +47,17 @@ class ItemsController(BaseController):
         self.respond_ok(handler, items)
 
     # /api/v1/items/{item_id}
-    def get_by_id(self, item_id, handler):
+    def get_by_id(self, handler, item_id):
         items = self.provider.fetch_item_pool().get_by_id(item_id)
         self.respond_ok(handler, items)
 
     # /api/v1/items/{item_id}/inventory
-    def get_inventory_by_item_id(self, item_id, handler):
+    def get_inventory_by_item_id(self, handler, item_id):
         inventories = self.provider.fetch_inventory_pool().get_all_by_item_id(item_id)
         self.respond_ok(handler, inventories)
 
     # /api/v1/items/{item_id}/inventory/totals
-    def get_inventory_totals_by_item_id(self, item_id, handler):
+    def get_inventory_totals_by_item_id(self, handler, item_id):
         totals = self.provider.fetch_inventory_pool().get_totals_by_item_id(item_id)
         self.respond_ok(handler, totals)
 
@@ -58,15 +69,13 @@ class ItemsController(BaseController):
         self.respond_created(handler, item)
 
     # /api/v1/items/{item_id}
-    def update(self, path, handler):
-        item_id = int(path[1])
+    def update(self, handler, item_id):
         content = self.get_json_body(handler)
         item = Item.from_dict(content)
         self.provider.fetch_item_pool().update(item_id, item)
         self.respond_ok(handler)
 
     # /api/v1/items/{item_id}
-    def delete(self, path, handler):
-        item_id = int(path[1])
+    def delete(self, handler, item_id):
         self.provider.fetch_item_pool().remove(item_id)
         self.respond_ok(handler)
