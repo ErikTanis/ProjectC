560b353dbddb5e41311c1f0bfc8a21ec5bb95413
Azif Rosan <azif_ownage@hotmail.com>
2024-10-07 14:53:24 +0200

    added endpoint path comments

--- Refactor/api/controllers/item_lines_controller.py
+++ Refactor/api/controllers/item_lines_controller.py
@@ -36,24 +36,29 @@ class ItemLinesController(BaseController):
         itemLineId = int(path[1])
         self.delete(path, handler, itemLineId)
 
+    # /api/v1/item_lines
     def get_all(self, handler):
         itemLines = self.provider.fetch_item_line_pool().get_all()
         self.respond_ok(handler, itemLines)
 
+    # /api/v1/item_lines/{item_line_id}
     def get_by_id(self, handler, item_id):
         itemLine = self.provider.fetch_item_line_pool().get_by_id(item_id)
         self.respond_ok(handler, itemLine)
 
+    # /api/v1/item_lines/{item_line_id}/items
     def get_items_by_item_line_id(self, handler, item_line_id):
         items = self.provider.fetch_item_pool().get_all_by_item_line(item_line_id)
         self.respond_ok(handler, items)
 
+    # /api/v1/item_lines/{item_line_id}
     def update(self, handler, item_line_id):
         data = self.get_json_body(handler)
         updated_item_line = ItemLine.from_dict(data)
         self.provider.fetch_item_line_pool().update(item_line_id, updated_item_line)
         self.respond_ok(handler)
 
+    # /api/v1/item_lines/{item_line_id}
     def delete(self, handler, item_line_id):
         self.provider.fetch_item_line_pool().remove(item_line_id)
         self.respond_no_content(handler)
